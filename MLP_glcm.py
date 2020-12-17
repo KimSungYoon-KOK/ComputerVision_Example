ce7d9dd20d7416048052cc47b32ce7a6bca95fb7
KimSungYoon-KOK <53232440+KimSungYoon-KOK@users.noreply.github.com>
2020-12-17 23:28:51 +0900

    commit

--- MLP_glcm.py
+++ MLP_glcm.py
@@ -52,7 +52,7 @@ def laws_texture(gray_image):
     return TEM
 
 
-# === 이미지 패치에서 특징 추출 ===
+# ================= 이미지 패치에서 특징 추출 =======================
 train_dir = './texture_data/train'
 test_dir = './textire_data/test'
 classes = ['brick', 'grass', 'ground']
@@ -102,3 +102,143 @@ X_test = np.array(X_test)
 Y_test = np.array(Y_test)
 print('test data: ', X_test.shape)
 print('test label: ', Y_test.shape)
+
+
+# === 신경망에 필요한 모듈 ===
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from torch.utils.data import DataLoader
+from torch.utils.data.dataset import Dataset
+from torchsummary import summary
+
+# === 데이터셋 클래스 ===
+class textureDataset(Dataset):
+    def __init__(self, features, labels):
+        self.features = features
+        self.labels = labels
+    
+    def __len__(self):
+        return len(self.labels)
+    
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
+        feature = self.features[idx]
+        label = self.labels[idx]
+        sample = (feature, label)
+
+        return sample
+
+# === 신경망 모델 클래스 ===
+class MLP(nn.module):
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        super(MLP, self).__init__()
+        self.fc1 = nn.Linear(input_dim, hidden_dim)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_dim, hidden_dim)
+        self.fc3 = nn.Linear(hidden_dim, output_dim)
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        out = self.relu(out)
+        out = self.fc3(out)
+
+        return out
+
+
+
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+batch_size = 10
+learning_rate = 0.01
+n_epoch = 500
+
+Train_data = textureDataset(features=X_train, labels=Y_train)
+Test_data = textureDataset(features=X_test, labels=Y_test)
+
+Trainloader = DataLoader(Train_data, batch_size=batch_size, shuffle=True)
+Testloader = DataLoader(Test_data, batch_size=batch_size)
+
+net = MLP(11, 8, 3)
+net.to(device)
+summary(net, (11,), device='cuda' if torch.cuda.is_available() else 'cpu')
+
+optimizer = optim.SGD(net.parameters(), lr=learning_rate)
+criterion = nn.CrossEntropyLoss()
+
+train_losses = []
+train_accs = []
+test_losses = []
+test_accs = []
+
+
+
+# ========== 학습 ==========
+for epoch in range(n_epoch):
+    train_loss = 0.0
+    evaluation = []
+    net.train()
+    for i, data in enumerate(Trainloader, 0):
+        features, labels = data
+        labels = labels.long().to(device)
+        features = features.to(device)
+        optimizer.zero_grad()
+
+        outputs = net(features.to(torch.float))
+        _, predicted = torch.max(outputs.cpu().data, 1)
+        evaluation.append((predicted==labels.cpu()).tolist())
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+
+    train_loss = train_loss/(i+1)
+    evaluation = [item for sublist in evaluation for item in sublist]
+    train_acc = sum(evaluation)/len(evaluation)
+
+    train_losses.append(train_loss)
+    train_accs.append(train_acc)
+
+
+    # === 테스트 ===
+    if(epoch+1)%1 == 0:
+        test_loss = 0.0
+        evaluation = []
+        net.eval()
+        for i, data in enumerate(Testloader, 0):
+            features, labels = data
+            labels = labels.long().to(device)
+            features = features.to(device)
+
+            outputs = net(features.to(torch.float))
+            _, predicted = torch.max(outputs.cpu().data, 1)
+            evaluation.append((predicted==labels.cpu()).tolist())
+            loss = criterion(outputs, labels)
+            test_loss += loss.item()
+        
+        test_loss = test_loss/(i+1)
+        evaluation = [item for sublist in evaluation for item in sublist]
+        test_acc = sum(evaluation)/len(evaluation)
+
+        test_losses.append(test_loss)
+        test_accs.append(test_acc)
+
+        print('[%d, %3d]\tloss: %.4f\tAccuracy : %.4f\t\tval-loss: %.4f\tval-Accuracy: %.4f' %
+                (epoch+1, n_epoch, train_loss, train_acc, test_loss, test_acc))
+
+
+
+# ===== 학습/테스트 loss/정확도 시각화 =====
+plt.plot(range(len(train_losses)), train_losses, label='train loss')
+plt.plot(range(len(test_losses)), test_losses, label='test loss')
+plt.legend()
+plt.show()
+plt.plot(range(len(train_accs)), train_accs, label='train acc')
+plt.plot(range(len(test_accs)), test_accs, label='test acc')
+plt.legend()
+plt.show()
+
